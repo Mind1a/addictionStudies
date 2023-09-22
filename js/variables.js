c4513b1b4a3d7ed165e9484d618d836de98fe900
teklaGogua <tekla.gogua.2006@gmail.com>
2023-09-22 21:50:33 +0400

    feature/resursebiPages
    Adding and styling inside pages for 'resursebi' page

---
+++ js/variables.js
@@ -0,0 +1,5 @@
+export const btnNav = document.querySelector(".btn-mobile-nav");
+export const container = document.querySelector(".container");
+
+export const resursebiList = document.querySelector(".resursebi-list");
+export const mainBox = document.querySelector(".main-box");
